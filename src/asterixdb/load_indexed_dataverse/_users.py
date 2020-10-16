92aa21ebb63bae2238104997b88de1f0478175ef
Glenn <gjgalvizo@gmail.com>
2020-10-16 10:53:29 -0700

    Modified framework for our executor now (using invoke instead of __call__, now operate on individual dataverses instead of both at same time). This has been applied to Users dataverse load.

--- src/asterixdb/load_indexed_dataverse/_users.py
+++ src/asterixdb/load_indexed_dataverse/_users.py
@@ -7,9 +7,14 @@ logger = logging.getLogger(__name__)
 
 
 class LoadIndexedUsersDataverse(AbstractBenchmarkRunnable):
+    # PATH_PREFIX = "localhost:///Users/glenngalvizo/Documents/Projects/asterixdb/ilima-repo/resources/"
+    # SARR_PATH = PATH_PREFIX + "SARR-UsersSample.json"
+    # ATOM_PATH = PATH_PREFIX + "ATOM-UsersSample.json"
+    PATH_PREFIX = "dbh-2074.ics.uci.edu:///home/ggalvizo/ilima/resources/"
+    SARR_PATH = PATH_PREFIX + "SARR-UsersFull.json"
+    ATOM_PATH = PATH_PREFIX + "ATOM-UsersFull.json"
+
     def __init__(self):
-        self.sarr_json = "dbh-2074.ics.uci.edu:///home/ggalvizo/ilima/resources/full_data/SARR-UsersFull.json"
-        self.atom_json = "dbh-2074.ics.uci.edu:///home/ggalvizo/ilima/resources/full_data/ATOM-UsersFull.json"
         super().__init__()
 
     def _benchmark_load_sarr(self):
@@ -20,12 +25,13 @@ class LoadIndexedUsersDataverse(AbstractBenchmarkRunnable):
 
             CREATE TYPE UsersType AS { user_id: string };
             CREATE DATASET Users (UsersType) PRIMARY KEY user_id;
+            CREATE DATASET UsersBuffer (UsersType) PRIMARY KEY user_id;
             LOAD DATASET ShopALot.SARR.Users USING localfs (
                 ("path"="%s"), ("format"="json")
             );
 
             CREATE INDEX usersNumberIdx ON Users(UNNEST phones SELECT number : string ?);
-        """ % self.sarr_json)
+        """ % self.SARR_PATH)
         self.log_results(results)
 
     def _benchmark_load_atom(self):
@@ -36,24 +42,23 @@ class LoadIndexedUsersDataverse(AbstractBenchmarkRunnable):
 
             CREATE TYPE UsersType AS { user_id: string };
             CREATE DATASET Users (UsersType) PRIMARY KEY user_id;
+            CREATE DATASET UsersBuffer (UsersType) PRIMARY KEY user_id;
             LOAD DATASET ShopALot.ATOM.Users USING localfs (
                 ("path"="%s"), ("format"="json")
             );
 
             CREATE INDEX usersNumberIdx ON Users (phone.number : string ?);
-        """ % self.atom_json)
+        """ % self.ATOM_PATH)
         self.log_results(results)
 
     def perform_benchmark(self):
-        logger.info('Executing load_indexed_dataverse on Users for SARR.')
-        self._benchmark_load_sarr()
-
-        logger.info('Executing load_indexed_dataverse on Users for ATOM.')
-        self._benchmark_load_atom()
-
-    def perform_post(self):
-        pass
+        if self.dataverse == self.SARR_DATAVERSE:
+            logger.info('Executing load_indexed_dataverse on Users for SARR.')
+            self._benchmark_load_sarr()
+        else:
+            logger.info('Executing load_indexed_dataverse on Users for ATOM.')
+            self._benchmark_load_atom()
 
 
 if __name__ == '__main__':
-    LoadIndexedUsersDataverse()()
+    LoadIndexedUsersDataverse().invoke()
