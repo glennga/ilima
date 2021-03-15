67effe9b998ec7b388140986e4e249a6ab76dff6
Glenn <gjgalvizo@gmail.com>
2021-03-15 11:44:29 -0700

    Similar to AsterixDB, we should avoid the serialization overhead when testing index maintenance.

--- src/couchbase/lower_bound/_insert.py
+++ src/couchbase/lower_bound/_insert.py
@@ -13,12 +13,20 @@ class LowerBoundInsert(AbstractLowerBoundRunnable):
         logger.info('Building bucket.')
         self.initialize_bucket()
 
+        logger.info('Initialize the buffer.')
+        self.execute_n1ql(f"""
+            INSERT INTO `{self.bucket_name}` (KEY, VALUE)
+            VALUES ("{str(uuid.uuid4())}", {{ "type": "TestCollectionBuffer", "a": 1 }});
+        """)
+
         logger.info('Now executing the lower bound statement.')
         for i in range(self.NUMBER_OF_REPEATS):
             logger.debug(f'Executing run {i + 1} for the lower bound statement.')
             results = self.execute_n1ql(f"""
-                INSERT INTO `{self.bucket_name}` (KEY, VALUE)
-                VALUES ("{str(uuid.uuid4())}", {{ "type": "TestCollection", "a": 1 }});
+                INSERT INTO `{self.bucket_name}` (KEY UUID(), VALUE _document)
+                SELECT _document
+                FROM `{self.bucket_name}`
+                WHERE `type` = "TestCollectionBuffer";
             """)
             results['runNumber'] = i + 1
             self.log_results(results)
